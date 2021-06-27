f046d6e7f8d0bc05ffedadc3716f13ef5cc9dec9
Diogenes <diogenes_mj@hotmail.com>
2021-06-27 11:39:06 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const morgan = require('morgan')
+const helmet =  require('helmet')
+const cors = require('cors')
+const app = express()
+
+app.use(morgan("common"))
+app.use(helmet())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.get('/',(req,res)=>{
+    res.status(200).json(req.body)
+})
+
+app.listen(3000,()=>{
+    console.log('On Start!')
+})
