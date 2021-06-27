a1a09da2a09f85a4f3eaf6750a76621d8d41a705
Diogenes <diogenes_mj@hotmail.com>
2021-06-27 14:29:01 -0400

    add: dotenv

--- main.js
+++ main.js
@@ -2,17 +2,21 @@ const express = require('express')
 const morgan = require('morgan')
 const helmet =  require('helmet')
 const cors = require('cors')
-const app = express()
+const dotenv = require('dotenv')
+dotenv.config()
 
-app.use(morgan("common"))
+const app = express()
+const PORT = process.env.PORT
+
+app.use(morgan("combined"))
 app.use(helmet())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 app.get('/',(req,res)=>{
-    res.status(200).json(req.body)
+    res.status(200).json({'message':'success'})
 })
 
-app.listen(80,()=>{
+app.listen(PORT,()=>{
     console.log('On Start!')
 })
